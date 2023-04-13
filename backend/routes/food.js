6139f1c62699624174b46d15f6807781f8bfeec3
Julian Lum <lumjulian@gmail.com>
2023-04-13 00:08:58 -0700

    test id

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const food = {
+const foods = {
   item1 : 'chinese', 
   item2 : 'korean',
   item3 : 'italian'
@@ -10,9 +10,14 @@ const food = {
 
   
   router.get('', (req,res) => {
-    const arr = Object.values(food)
+    const arr = Object.values(foods)
     res.json(arr)
   })
 
+  router.get('./foodId', (req,res) => {
+    const foodId = req.params.foodId
+    const food = foods[foodId]
+    res.json(food)
+  })
 
   module.exports = router;
